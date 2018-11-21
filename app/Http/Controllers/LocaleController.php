17f186b2a5c2b56da45a3a49b2411c9ab50b766d
Clément Rigo <rigoclement@mydnic.be>
2018-11-21 09:43:44 +0100

    refactor locale switcher controller

--- app/Http/Controllers/LocaleController.php
+++ app/Http/Controllers/LocaleController.php
@@ -7,50 +7,62 @@ use Illuminate\Support\Facades\Lang;
 
 class LocaleController extends Controller
 {
+    protected $previousRequest;
+    protected $locale;
+
     public function switch($locale)
     {
-        $previousRequest = $this->getPreviousRequest();
-
-        // Get Query Parameters if applicable
-        $queryBag = $previousRequest->query();
+        $this->previousRequest = $this->getPreviousRequest();
+        $this->locale = $locale;
 
         // Store the segments of the last request as an array
-        $segments = $previousRequest->segments();
+        $segments = $this->previousRequest->segments();
 
         // Check if the first segment matches a language code
-        if (array_key_exists($locale, config('app.locales'))) {
+        if (array_key_exists($this->locale, config('app.locales'))) {
             // Replace the first segment by the new language code
-            $segments[0] = $locale;
+            $segments[0] = $this->locale;
 
-            $newRoute = $this->translateRouteSegments($segments, $locale);
+            $newRoute = $this->translateRouteSegments($segments);
 
             // Redirect to the required URL
-            $redirectUrl = implode('/', $newRoute->toArray());
-            $redirectUrl .= count($queryBag) ? '?' . http_build_query($queryBag) : '';
-            return redirect()->to($redirectUrl);
+            return redirect()->to($this->buildNewRoute($newRoute));
         }
 
         return back();
     }
 
-    private function getPreviousRequest()
+    protected function getPreviousRequest()
     {
         // We Transform the URL on which the user was into a Request instance
         return request()->create(url()->previous());
     }
 
-    private function translateRouteSegments($segments, string $locale)
+    protected function translateRouteSegments($segments)
     {
         $translatedSegments = collect();
 
         foreach ($segments as $segment) {
             if ($key = array_search($segment, Lang::get('routes'))) {
-                $translatedSegments->push(trans('routes.' . $key, [], $locale));
+                // The segment exists in the translations, so we will grab the translated version.
+                $translatedSegments->push(trans('routes.' . $key, [], $this->locale));
             } else {
+                // Otherwise we simply reuse the same.
                 $translatedSegments->push($segment);
             }
         }
 
         return $translatedSegments;
+    }
+
+    protected function buildNewRoute($newRoute)
+    {
+        $redirectUrl = implode('/', $newRoute->toArray());
+
+        // Get Query Parameters if any, so they are preserved
+        $queryBag = $this->previousRequest->query();
+        $redirectUrl .= count($queryBag) ? '?' . http_build_query($queryBag) : '';
+
+        return $redirectUrl;
     }
 }
